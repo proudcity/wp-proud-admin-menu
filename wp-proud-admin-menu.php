936ea7f96ffa8487ba837496395cdf048384a83f
Curtis McHale <curtis@curtismchale.ca>
2023-09-19 07:16:26 -0700

    finished fixing the issues raised by luke

--- wp-proud-admin-menu.php
+++ wp-proud-admin-menu.php
@@ -3,7 +3,7 @@
 Plugin Name: ProudCity Admin Menu
 Plugin URI: https://proudcity.com
 Description: Builds out the WP Admin Menu in the order we want with the styles to suit our theme
-Version: 2023.09.14.1338
+Version: 2023.09.19.0715
 Author: ProudCity
 Author URI: https://proudcity.com
 License: GPLv2 or later
