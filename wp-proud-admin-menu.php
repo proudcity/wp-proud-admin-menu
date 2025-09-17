b474395b08b71e4d99dc959e1738045f28a6d6dc
Curtis McHale <curtis@curtismchale.ca>
2025-09-17 12:16:54 -0700

    bumping version number

--- wp-proud-admin-menu.php
+++ wp-proud-admin-menu.php
@@ -3,7 +3,7 @@
 Plugin Name: ProudCity Admin Menu
 Plugin URI: https://proudcity.com
 Description: Builds out the WP Admin Menu in the order we want with the styles to suit our theme
-Version: 2025.09.10.1342
+Version: 2025.09.17.1216
 Author: ProudCity
 Author URI: https://proudcity.com
 License: GPLv2 or later
